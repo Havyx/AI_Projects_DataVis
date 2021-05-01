69959fbb91c12c0461a5e4360ab99a5c0ae95932
havyx <everluca@outlook.com>
2021-05-01 09:40:35 -0300

    cleaning folders

--- django_projects/dash_admin_project/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('index', views.home, name='home'),
-    path('', views.home, name='home'),
-    path('app', views.base, name='app'),
-]
